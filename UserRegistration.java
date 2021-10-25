5ef7891ec76c9323d5890e2a944fa01823060a1f
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-25 12:57:05 +0530

    Validate Phone Number

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,9 +7,9 @@ public class UserRegistration {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Email Id : ");
-        String getEmail = sc.next();
-        UC3 email = new UC3();
-        email.email(getEmail);
+        System.out.println("Enter Phone Number : ");
+        String getUserInput = sc.next();
+        UC4 phoneNumber = new UC4();
+        phoneNumber.validate(getUserInput);
     }
 }
