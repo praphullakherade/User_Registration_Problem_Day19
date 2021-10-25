2dbc2f155cbd45a76f5c8930015aa1a9a8c618b8
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-25 12:33:30 +0530

    Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,9 +7,9 @@ public class UserRegistration {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Last Name : ");
-        String getLastName = sc.next();
-        UC2 last = new UC2();
-        last.lastName(getLastName);
+        System.out.println("Enter Email Id : ");
+        String getEmail = sc.next();
+        UC3 email = new UC3();
+        email.email(getEmail);
     }
 }
