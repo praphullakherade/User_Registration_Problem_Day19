6a91215d4f8e8952eabe8842f206635dcc522218
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-25 12:01:04 +0530

    Welcome to User Registration Problem

---
+++ UserRegistration.java
@@ -0,0 +1,8 @@
+package com.userregistration;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Problem");
+    }
+}
