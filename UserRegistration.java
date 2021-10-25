f9ba841cc48ceb25412eb1379b4c4cd0ccb4f665
Praphulla Kherade <prafullkherade9@gmail.com>
2021-10-25 12:15:11 +0530

    First name starts with Cap and has minimum 3 characher

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,15 @@
 package com.userregistration;
 
+import java.util.Scanner;
+
 public class UserRegistration {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Problem");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Name : ");
+        String getFirstName = sc.next();
+        UC1 first = new UC1();
+        first.firstName(getFirstName);
     }
 }
