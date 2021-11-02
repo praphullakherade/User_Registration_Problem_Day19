ed6ed0edb8681764e2de40cb78b6a48ded75f821
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-02 18:50:41 +0530

    Email Samples

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,13 +3,41 @@ package com.userregistration;
 import java.util.Scanner;
 
 public class UserRegistration {
+    Scanner scanner = new Scanner(System.in);
+    UserDetails userDetails = new UserDetails();
 
-    public static void main(String[] args) {
+    public void userFirstName() {
+        System.out.println("Enter Your First Name : ");
+        String firstName = scanner.next();
+        userDetails.firstName(firstName);
+    }
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter password : ");
-        String getUserInput = sc.next();
-        UC8 uc8 = new UC8();
-        uc8.validate(getUserInput);
+    public void userLastName() {
+        System.out.println("Enter Your Last Name : ");
+        String lastName = scanner.next();
+        userDetails.lastName(lastName);
+    }
+
+    public void userEmailId() {
+        System.out.println("Enter Your EmailId : ");
+        String emailId = scanner.next();
+        userDetails.email(emailId);
+    }
+
+    public void userMobileNumber() {
+        System.out.println("Enter your MobileNumber");
+        String phoneNumber = scanner.next();
+        userDetails.phoneNumber(phoneNumber);
+    }
+
+    public void userPassword() {
+        System.out.println("Enter your MobileNumber");
+        String password = scanner.next();
+        userDetails.password(password);
+    }
+
+    public void sampleEmailIds() {
+        TestSampleEmailIds testSampleEmailIds = new TestSampleEmailIds();
+        testSampleEmailIds.testForValidEmails();
     }
 }
