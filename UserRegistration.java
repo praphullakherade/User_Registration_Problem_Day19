74a1c6abd5f8ea5c8d1e34cfad1efcbb11d16dce
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-02 18:29:32 +0530

    Password Rule- at least 1 Upeer Case

--- UserRegistration.java
+++ UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter password : ");
         String getUserInput = sc.next();
-        UC5 uc5 = new UC5();
-        uc5.validate(getUserInput);
+        UC6 uc6 = new UC6();
+        uc6.validate(getUserInput);
     }
 }
